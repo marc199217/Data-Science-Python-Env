634dd1fd1cfa9901b14be10612612d29ac50670a
Marc Nordbruch <60784007+marc199217@users.noreply.github.com>
2021-05-23 23:22:38 +0200

    Initial commit

---
+++ app/install-packages.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Bash "strict mode", to help catch problems and bugs in the shell
+# script. Every bash script you write should include this. See
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/ for
+# details.
+set -euo pipefail
+
+# Tell apt-get we're never going to be able to give manual
+# feedback:
+export DEBIAN_FRONTEND=noninteractive
+
+# Update the package listing, so we know what package exist:
+apt-get update
+
+# Install security updates:
+apt-get -y upgrade
+
+# Install a new package, without unnecessary recommended packages:
+apt-get -y install --no-install-recommends build-essential pkg-config apt-utils libpq-dev python3-dev libxml2-dev libxmlsec1-dev libhdf5-dev
+# Delete cached files we don't need anymore:
+apt-get clean
+rm -rf /var/lib/apt/lists/*
